0e6068c8947d2fbeb84e022cac11906bb3d97847
Deniss <den.b@itservicesgroup.co.uk>
2013-09-30 18:23:23 +0100

    Rockpapescissors app web styling added and javascript func

---
+++ RockPaperScissors/web/game.js
@@ -0,0 +1,15 @@
+// var player1 = new Player();
+// var player1Move = document.getElementById('move').value;
+// // $('#move').val() // jQuery way
+// player1.picks(player1Move);
+// var game = new Game(player1, player2);
+// console.log(game.winner());
+function addMessage() {
+	document.getElementById("message").innerHTML = "My Message"
+}
+
+window.onload = addMessage;
+
+$("input").submit(function() {
+	
+})
